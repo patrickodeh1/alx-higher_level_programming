dbf6491fb415e3c2079e29c3b3feb8e135c279c9
patrick Odeh <vyltyan@gmail.com>
2024-04-18 14:07:13 +0100

    0-safe_print_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     new = my_list[:x]
+    num = 0
     for i in new:
+        num = i
         try:
             print("{}".format(i), end='')
-        except:
+        except(ValueError):
             pass
     print()
-    return i
+    return num
