161325c5b91d607af9e7ca56f9c1d48ca93b17ad
patrick Odeh <vyltyan@gmail.com>
2024-04-18 14:37:24 +0100

    2-print

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+def length(words):
+    count = 0
+    for i in words:
+        count += 1
+    return count
+
 def safe_print_list_integers(my_list=[], x=0):
     new = my_list[:x]
     val = []
     for i in new:
-        if x <= len(my_list):
+        if x <= length(new):
             try:
                 print("{:d}".format(i), end='')
                 val.append(i)
@@ -12,4 +18,4 @@ def safe_print_list_integers(my_list=[], x=0):
         else:
             raise IndexError
     print()
-    return len(val)
+    return length(val)
