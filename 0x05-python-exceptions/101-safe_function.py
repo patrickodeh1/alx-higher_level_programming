725cac6d9cac2e471938020f299549be999d3926
patrick Odeh <vyltyan@gmail.com>
2024-05-01 11:08:49 +0100

    corrections

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_function(fct, *args):
     try:
         return fct(*args)
     except Exception as n:
-       print("Exception: {}".format(n))
+       print("Exception: {}".format(n), file=sys.stderr)
        return None
