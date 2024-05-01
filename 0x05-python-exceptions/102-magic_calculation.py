a225661143d9d4d982c387a045bc07fd24d87220
patrick Odeh <vyltyan@gmail.com>
2024-05-01 11:24:10 +0100

    corrections

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+        except:
+            pass
+        result += (a + b) ** i / i
+    result += b + a
