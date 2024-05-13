e9d823e0d85a331e2ec2ab635cd54850370e7824
patrick Odeh <vyltyan@gmail.com>
2024-05-13 16:17:44 +0100

    doc test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""sums up two integers"""
+
+
 def add_integer(a, b=98):
+    """Adds two integers"""
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     elif not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
     if isinstance(a, float):
         a = int(a)
-    if isinstance(b,float):
+    if isinstance(b, float):
         b = int(b)
     return a + b
