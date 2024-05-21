f9e29b4aa553f1b87564d51e8ab980ee24108c2f
Patrick Odeh <vyltyan@gmail.com>
2024-05-21 12:43:08 -0700

    sql

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,5 @@
 -- script that displays the number of records 
 -- script that displays the number of records
-SELECT COUNT(89) AS id FROM first_table;
+SELECT COUNT(*) 
+FROM first_table
+WHERE id = 89;
