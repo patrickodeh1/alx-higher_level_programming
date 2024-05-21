e72a452a52a2f5ba2fc1cb30a96f67867de5846b
Patrick Odeh <vyltyan@gmail.com>
2024-05-21 13:18:36 -0700

    sql

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,7 +1,3 @@
 -- computes averages
 -- computes score average
-SELECT AVG(id) AS avg_id, AVG(score) AS avg_score, AVG(name_length) AS avg_name_length
-FROM (
-    SELECT id, score, LENGTH(name) AS name_length
-    FROM second_table
-) AS subquery;
+SELECT AVG(score) AS average FROM second_table;
