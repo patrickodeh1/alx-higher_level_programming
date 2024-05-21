c88f10fc59048bbc746dd5ce4a843cee352d263f
Patrick Odeh <vyltyan@gmail.com>
2024-05-21 08:17:19 -0700

    sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,6 @@
 -- Prints full description
 -- full description of table first_table
-DESCRIBE hbtn_0c_0.first_table;
+SELECT COLUMN_NAME AS 'Field', COLUMN_TYPE AS 'Type', IS_NULLABLE AS 'Null', COLUMN_KEY AS 'Key', COLUMN_DEFAULT AS 'Default', EXTRA AS 'Extra'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0'
+AND TABLE_NAME = 'first_table';
