8237a0b29f5ff4057601efdc6868149b4ab62a73
patrick Odeh <vyltyan@gmail.com>
2024-05-22 13:56:46 +0100

    sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,9 +1,8 @@
 -- Import the temperatures table data
 -- Display the top 3 cities by average temperature during July and August
-USE hbtn_0c_0;
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
-WHERE MONTH(date) IN (7, 8)
+WHERE month IN ('July', 'August')
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
