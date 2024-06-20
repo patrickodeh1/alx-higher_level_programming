15d55206fd7acb36f7f6de8a4a7b50f4cee36dbd
Patrick Odeh <vyltyan@gmail.com>
2024-06-20 11:37:47 +0100

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -5,5 +5,7 @@ const args = process.argv.slice(2);
 if (args.length === 0) {
   console.log('No argument');
 } else {
-  console.log(args);
+  args.foreach((arg) => {
+    console.log(`${args}`);
+  });
 }
