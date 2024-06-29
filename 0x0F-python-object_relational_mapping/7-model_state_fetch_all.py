4ea4ae34610c2104424b1df1755662931a606502
Patrick Odeh <vyltyan@gmail.com>
2024-06-29 19:53:47 +0100

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,22 +1,34 @@
 #!/usr/bin/python3
 
-import SQLAlchemy
-from model_state import State, session
 """
 This module connects to a MySQL database and lists all State objects.
 It uses SQLAlchemy ORM to interact with the database.
 """
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import State, Base
 
 
 def list_states():
     """
          Fetches all State records from the database and prints them.
     """
-    states = session.query(state).all()
+    states = session.query(State).order_by(State.id).all()
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
 
 
 if __name__ == "__main__":
-    list_states()
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.close()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -21,5 +21,3 @@ if __name__ == "__main__":
             '.format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
     Base.metadata.create_all(engine)
-    session = sessionmaker(bind=engine)
-    session = Session()
