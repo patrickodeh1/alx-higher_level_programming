2a92b9380a3e374d28ed352cb34cdaf33afd4b5a
patrickodeh1 <136114171+patrickodeh1@users.noreply.github.com>
2024-06-29 19:37:44 +0100

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""state model"""
+""" state model"""
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
