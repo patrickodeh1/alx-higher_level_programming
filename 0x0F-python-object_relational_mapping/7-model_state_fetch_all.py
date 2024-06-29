1b04c4239f9463755453a1df01d8276a30af619e
Patrick Odeh <vyltyan@gmail.com>
2024-06-29 19:07:59 +0100

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
+import SQLAlchemy
+from model_state import State, session
 """
 This module connects to a MySQL database and lists all State objects.
 It uses SQLAlchemy ORM to interact with the database.
 """
 
-import SQLAlchemy
-from model_state import State, session
 
 
 def list_states():
