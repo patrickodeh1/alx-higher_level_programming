500a28e4ca26b79673cf4195147fd5ab528de412
Patrick Odeh <vyltyan@gmail.com>
2024-06-30 03:10:11 +0100

    sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+This module connects to a MySQL database and lists all State objects.
+It uses SQLAlchemy ORM to interact with the database.
+"""
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        username, password, db_name), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+
+    print("{}".format(new_state.id))
+
+    session.close()
