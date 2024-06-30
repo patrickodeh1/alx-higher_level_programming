5ec0377c9c75a04cb24aed2371b360403041d9ef
Patrick Odeh <vyltyan@gmail.com>
2024-06-30 03:16:38 +0100

    sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+This module connects to a MySQL database and lists all State objects.
+It uses SQLAlchemy ORM to interact with the database.
+"""
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        username, password, db_name), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(id=2).first()
+    if state:
+        state.name = "New Mexico"
+        session.commit()
+
+    session.close()
