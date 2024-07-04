791b970c8606fb4a039d7f7cb5dffa46f4b37ffc
Patrick Odeh <vyltyan@gmail.com>
2024-07-04 21:00:15 +0100

    2-delete.sh

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,16 @@
+#!/usr/bin/python3
+"""Module to find a peak in a list of unsorted integers"""
+
+
 def find_peak(list_of_integers):
-    if len(list_of_integers) == 0:
+    """Find a peak in a list of unsorted integers"""
+    if not list_of_integers:
         return None
-    elif len(list_of_integers) == 1:
-        return list_of_integers[0]
-    else:
-        mid = len(list_of_integers) // 2
-        if list_of_integers[mid] > list_of_integers[mid - 1] \
-                and list_of_integers[mid] > list_of_integers[mid + 1]:
-            return list_of_integers[mid]
-        elif list_of_integers[mid] < list_of_integers[mid - 1]:
-            return find_peak(list_of_integers[:mid])
+    left, right = 0, len(list_of_integers) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
         else:
-            return find_peak(list_of_integers[mid + 1:])
+            right = mid
+    return list_of_integers[left]
