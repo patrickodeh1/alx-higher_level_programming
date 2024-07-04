773a5984cbd5c0aa386dace27de0ae4f92a02f6d
Patrick Odeh <vyltyan@gmail.com>
2024-07-04 20:53:19 +0100

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-url=$1
-
-size=$(curl -s -o /dev/null -w "%{size_download}\n" "$url")
-
-echo "$size"
+# Sends a request to a URL and displays the size of the response body
+curl -s "$1" | wc -c
