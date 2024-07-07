44bb062214f5712b35453c56fa9deba8fb6ec37d
Patrick Odeh <vyltyan@gmail.com>
2024-07-07 13:50:02 +0100

    tests

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -10,13 +10,13 @@ def matrix_divided(matrix, div):
     """
     if not isinstance(matrix, list) or\
             not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list\
-        of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
 
     if not all(isinstance(elem, (int, float))
                for row in matrix for elem in row):
-        raise TypeError("matrix must be a matrix (list of\
-        lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
 
     row_length = len(matrix[0])
     if not all(len(row) == row_length for row in matrix):
