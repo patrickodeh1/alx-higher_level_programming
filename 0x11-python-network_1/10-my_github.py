5ec7d47e5ffb4e645e66b75fbf9dca1f9ac343e7
Patrick Odeh <vyltyan@gmail.com>
2024-07-07 07:49:32 +0100

    Network

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,16 +9,8 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
 
-    url = f'https://api.github.com/users/{username}'
-    headers = {'Accept': 'application/vnd.github.v3+json'}
+    url = 'https://api.github.com/user'
 
-    try:
-        r = requests.get(url, auth=(username, password), headers=headers)
-        if r.status_code == 200:
-            user_data = r.json()
-            print(user_data['id'])
-        else:
-            print(None)
-
-    except requests.exceptions.RequestException as e:
-        print(None)
+    r = requests.get(url, auth=(username, password))
+    user_data = r.json()
+    print(user_data.get('id'))
