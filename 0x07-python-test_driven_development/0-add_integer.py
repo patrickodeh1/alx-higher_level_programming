c574b70d4d9dc513a43428c8fbeae3dc89ab756b
Patrick Odeh <vyltyan@gmail.com>
2024-07-07 13:32:09 +0100

    tests

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,14 +6,16 @@ def add_integer(a, b=98):
     """Adds two integers"""
 
     if isinstance(a, float):
-        if a == float('inf') or a == float('-inf') or a != a:  # checks for inf and NaN
+        if a == float('inf') or \
+                a == float('-inf') or a != a:  # checks for inf and NaN
             raise TypeError("a must be an integer")
         a = int(a)
     elif not isinstance(a, int):
         raise TypeError("a must be an integer")
 
     if isinstance(b, float):
-        if b == float('inf') or b == float('-inf') or b != b:  # checks for inf and NaN
+        if b == float('inf') or \
+                b == float('-inf') or b != b:  # checks for inf and NaN
             raise TypeError("b must be an integer")
         b = int(b)
     elif not isinstance(b, int):
