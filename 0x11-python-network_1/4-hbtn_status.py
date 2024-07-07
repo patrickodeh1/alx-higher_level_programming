d668649292491bdf1108bbab45e98ab2ae1f81d6
Patrick Odeh <vyltyan@gmail.com>
2024-07-07 07:43:51 +0100

    Network

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,8 +5,7 @@ import requests
 
 
 if __name__ == "__main__":
-    r = requests.get('https://alx-intranet.hbtn.io/status\
-    ', auth=('user', 'pass'))
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     body = r.text
     print("Body response:")
     print("\t- type: {}".format(type(body)))
