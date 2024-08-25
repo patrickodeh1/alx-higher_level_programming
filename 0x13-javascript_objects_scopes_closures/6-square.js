c8ac5f368e5c4a8b44feab847c362758ba403f45
Patrick Odeh <vyltyan@gmail.com>
2024-08-25 16:40:23 +0100

    warm up

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
-const Square = require('./5-square');
+const Rectangle = require('./5-square');
 
-class Square extends Square {
-  charPrint(c) {
-    const char = c || 'X';  // Use 'X' if c is undefined
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    const char = c || 'X'; // Use 'X' if c is undefined
     for (let i = 0; i < this.width; i++) {
       console.log(char.repeat(this.width));
     }
