c32594b13ba8cad6672841af5ac30012a6a17730
Patrick Odeh <vyltyan@gmail.com>
2024-08-25 15:41:08 +0100

    warm up

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
-function factorial(n) {
-    if (n <= 1) {
-      return 1;
-    }
-  return n * factorial(n - 1);
+function factorial (n) {
+  if (n <= 1) {
+    return 1;
   }
+  return n * factorial(n - 1);
+}
 
 const num = parseInt(process.argv[2]);
-  
+
 console.log(factorial(isNaN(num) ? 1 : num));
-  
